f802dff03bab3b1bba3d89e959b702e77b5b5995
Katie <kamcconnell1@gmail.com>
2020-04-13 15:18:04 +0100

    9x9 grid game working, reset button still not working properly

--- scripts/app.js
+++ scripts/app.js
@@ -5,21 +5,24 @@ function init() {
   const cells = []
   const resetBtn = document.querySelector('#reset')
   const bombCounter = document.querySelector('#bomb-count')
+  const clickCounter = document.querySelector('#click-count')
 
   // * Grid Variables
   const width = 9
   const cellCount = width * width
   const bombCount = 10 //this will need to be updated to link to width when size increases 
-  const cell = 0
+  let clickCount = 0
 
   // * Game Variables
   let bombPosition = []
   let bombPositions = []
-  const hintPositions = []
+  // const adjCells = [cells[i - 10], cell[i - 9], cells[i - 8], cells[i - 1], cells[i + 1], cells[i + 8], cells[i + 9], cells[i + 10]]
 
-  // * Functions 
+
+  // *-------------------------- Functions On Page Load -------------------------------------
+
+  //----------------------------- Create the cells ------------------------------------------
   function createCells() {
-    // Create the cells
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       grid.appendChild(cell)
@@ -28,9 +31,9 @@ function init() {
     }
   }
 
+  //----------------------------- Position the bombs ----------------------------------------
   function positionBombs() {
     bombPositions = []
-    // Position the bombs
     for (let i = 0; i <= bombCount - 1; i++) {
       bombPosition = Math.floor(Math.random() * cellCount)
       if (cells[bombPosition].classList.contains('bomb')) {
@@ -41,16 +44,19 @@ function init() {
       }
       bombPositions.push(bombPosition)
     }
+    bombCounter.textContent = bombCount
+    clickCounter.textContent = clickCount
     console.log(`Bomb in divs ${bombPositions}, (${bombPositions.length})`)
   }
   
+  // -------------------------- Position the hints ------------------------------------------
   function positionHints() {
     // cells.forEach(cell => cell.textContent.replace(/[0-9]/g, ' '))
     // cells.forEach((cell) => {
     //   let hintArr = cell.textContent.split('')
     //   hintArr = []
     // })
-
+    
     cells.forEach((cell, i) => {
       // console.log('I am the cell', cell.textContent) 
       const x = cell.textContent % width
@@ -66,62 +72,73 @@ function init() {
         ${(x > 0 && cells[i + 8] && cells[i + 8].hasAttributes('.bomb')) ? 1 : 0} +
         ${(cells[i + 9] && cells[i + 9].hasAttributes('.bomb')) ? 1 : 0} +
         ${(x < width - 1 && cells[i + 10] && cells[i + 10].hasAttributes('.bomb')) ? 1 : 0}`).replace(/[^\d.-]/g, '').split('')
-
+        
         // console.log('value is', (value))
         const hintNum = value.reduce((a, b) => {
           return a + parseInt(b)
         },0)
         cell.textContent = hintNum > 0 ? hintNum : ''
+      
       }
     })
   }
   
-  
-  // function coverGrid() {
-  //   cells.map(cell => {
-  //     cell.classList.add('cover')
-  //   })
-  //   bombCounter.textContent = bombCount
-  // }
+  // --------------------------- Cover the Grid ---------------------------------------------
+  function coverGrid() {
+    cells.map(cell => {
+      cell.classList.add('cover')
+    })
+  }
  
+  // * -------------------- Functions for Playing the Game ----------------------------------
+
+  // -------------------------- Reveal Cell on Click ----------------------------------------
   function revealCell(event) {
+    clickCount += 1
+    clickCounter.textContent = clickCount
+
     // console.log(`just clicked ${event.target.textContent}`)
     if (event.target.classList.contains('bomb')) {
       cells.filter(cell => {
-        if (cell.classList.contains('bomb')) cell.classList.remove('cover') && cell.classList.remove('flag')
+        if (cell.classList.contains('bomb')) 
+          cell.classList.remove('cover', 'flag') 
       })
-    } else {
+    } else  {
       event.target.classList.remove('cover')
     }
   }
+  
 
+  // -------------------------- Add Flag on Right Click -------------------------------------
   function addFlag(event) {
     event.preventDefault()
     console.log(`just right clicked ${event}`)
     event.target.classList.add('flag')
 
+    clickCount += 1
     bombCounter.textContent -= 1
+    clickCounter.textContent = clickCount
   }
 
 
-
+  // ------------------------------ Reset the Game -------------------------------------------
   function resetGame() {
     console.log('the reset button was clicked')
-    // cells.map(cell => {
-    //   cell.classList.remove('bomb')
-    // }) //!   WHY DIDN'T THIS WORK?
     cells.forEach(cell => cell.classList.remove('bomb', 'flag'))
     // cells.forEach(cell => cell.textContent.replace(/\d/g, ''))
     positionBombs()
     positionHints()
-    // coverGrid()
+    coverGrid()
   }
 
+
+
+
   createCells()
-  // * Event Listeners
+  // *------------------------------ Event Listeners -----------------------------------------
   window.addEventListener('load', positionBombs)
   window.addEventListener('load', positionHints)
-  // window.addEventListener('load', coverGrid)
+  window.addEventListener('load', coverGrid)
   cells.forEach(cell => cell.addEventListener('click', revealCell))
   cells.forEach(cell => cell.addEventListener('contextmenu', addFlag))
   resetBtn.addEventListener('click', resetGame)
